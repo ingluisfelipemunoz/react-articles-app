ad0af493f68daa3546739cdf75856bc676fb189a
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 12:01:05 -0400

    base articles categories page

---
+++ src/features/articles/adapters/ui/pages/ArticlesCategoriesPage.tsx
@@ -0,0 +1,10 @@
+import Page from "../../../../../shared/ui/Page";
+
+export default function ArticlesCategoriesPage() {
+    return (
+        <Page>
+            <h1 className="text-2xl font-bold mb-4">Categories</h1>
+            {/* categorias y subcategorias */}
+        </Page>
+    );
+}
