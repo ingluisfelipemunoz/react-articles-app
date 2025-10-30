cfe0c9bb2a7d60dd2a4ed195ab4e3d91993dac71
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 10:50:26 -0400

    storage helper function

---
+++ src/shared/lib/storage.ts
@@ -0,0 +1,17 @@
+export function getJSON<T>(key: string, fallback: T): T {
+  try {
+    const raw = localStorage.getItem(key);
+    return raw ? (JSON.parse(raw) as T) : fallback;
+  } catch (error) {
+    console.error(error);
+    return fallback;
+  }
+}
+
+export function setJSON<T>(key: string, value: T) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error(error);
+  }
+}
