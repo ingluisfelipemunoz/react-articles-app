62e5da65e844f319128eb251c807e062501b198d
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 12:02:49 -0400

    article detail page

---
+++ src/features/articles/adapters/ui/pages/ArticleDetailPage.tsx
@@ -0,0 +1,10 @@
+import Page from "../../../../../shared/ui/Page";
+
+export default function ArticleDetailPage() {
+    return (
+        <Page>
+            <h1 className="text-2xl font-bold mb-44">Article Detail</h1>
+            {/* detalle del articulo con raiting favority etc*/}
+        </Page>
+    );
+}
