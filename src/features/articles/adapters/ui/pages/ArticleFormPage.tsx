e2f6b0f1619bd37e5f4b8e071465acfcf9a57601
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 12:04:28 -0400

    create-edit article form

---
+++ src/features/articles/adapters/ui/pages/ArticleFormPage.tsx
@@ -0,0 +1,10 @@
+import Page from "../../../../../shared/ui/Page";
+
+export default function ArticleFormPage() {
+    return (
+        <Page>
+            <h1>Create / Edit Article</h1>
+            {/* formulario para crear editar el artculo */}
+        </Page>
+    );
+}
