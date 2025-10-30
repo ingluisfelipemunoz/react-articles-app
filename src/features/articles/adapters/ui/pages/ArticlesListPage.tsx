d0c92cc048f22dcf40bbbdcb4565992cdcf0b18f
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 11:58:28 -0400

    styling ArticlesListPage

--- src/features/articles/adapters/ui/pages/ArticlesListPage.tsx
+++ src/features/articles/adapters/ui/pages/ArticlesListPage.tsx
@@ -5,12 +5,12 @@ import { Pagination } from "../components/Pagination";
 export default function ArticlesListPage() {
     return (
         <Page>
-            <div>
-                <h1>articles</h1>
+            <div className="flex items-center justify-between mb-4">
+                <h1 className="text-2xl font-bold">Articles</h1>
             </div>
-            <div><FiltersBar/></div>
-            <div>data</div>
-            <div><Pagination/></div>
+            <div className="mb-4"><FiltersBar/></div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-4">data</div>
+            <div className="mt-6 flex justify-end"><Pagination/></div>
         </Page>
     );
 }
