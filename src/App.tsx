45e2780272b9fe3d125d487b8dec57b44f1efb9e
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 09:58:13 -0400

    configuring tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50 text-slate-900">
+      <h1 className="text-3xl font-bold mb-4 text-brand">Tailwind v4 funcionando 🎨</h1>
+      <button className="px-4 py-2 rounded-lg bg-brand text-white hover:bg-brand/80 transition">
+        Tailwdind test
+      </button>
+    </div>
     </>
   )
 }
