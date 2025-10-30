f085cd55f7d2cc6ee2ee4927bb0c99ca2d6c61be
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 12:05:06 -0400

    styles for create article form

--- src/features/articles/adapters/ui/pages/ArticleFormPage.tsx
+++ src/features/articles/adapters/ui/pages/ArticleFormPage.tsx
@@ -3,7 +3,7 @@ import Page from "../../../../../shared/ui/Page";
 export default function ArticleFormPage() {
     return (
         <Page>
-            <h1>Create / Edit Article</h1>
+            <h1 className="text-2xl font-bold mb-4">Create / Edit Article</h1>
             {/* formulario para crear editar el artculo */}
         </Page>
     );
