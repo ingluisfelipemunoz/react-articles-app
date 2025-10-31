ab9ae6ee1c71aa6787aefa786dbb425ff38bb25d
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-31 00:07:27 -0400

    fetching articles and showing items with the articleCard component

--- src/features/articles/adapters/ui/pages/ArticlesListPage.tsx
+++ src/features/articles/adapters/ui/pages/ArticlesListPage.tsx
@@ -1,20 +1,31 @@
 import Page from "../../../../../shared/ui/Page";
+import { useArticlesParams } from "../../../application/useArticlesParams";
+import { useListArticles } from "../../../application/useListArticles";
+import { ArticleCard } from "../components/ArticleCard";
 import { FiltersBar } from "../components/FiltersBar";
 import { Pagination } from "../components/Pagination";
 
 export default function ArticlesListPage() {
+    const {page, pageSize, q, categoryId, subcategoryId, set} = useArticlesParams();
+    const {data, isLoading, error} = useListArticles({page, pageSize, q, categoryId, subcategoryId});
+
+
     return (
         <Page>
             <div className="flex items-center justify-between mb-4">
                 <h1 className="text-2xl font-bold">Articulos</h1>
             </div>
             <div className="mb-4"><FiltersBar/></div>
+            {/* lista de articulos */}
+            {!isLoading && !error && data && (<>
+            {
+            data.items.length === 0 ? 
+            (<div className="p4 rounded-xl border bg-white">Sin resultados...</div>) 
+            : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-4">
-                <div className="rounded-xl border bg-white p-4 shadow-sm">Articulo 1</div>
-                <div className="rounded-xl border p-4 bg-white shadow-sm">Articulo 2</div>
-                <div className="rounded-xl border p-4 bg-white shadow-sm">Articulo 3</div>
-                <div className="rounded-xl border p-4 bg-white shadow-sm">Articulo 4</div>
-            </div>
+                {data.items.map(item => (<ArticleCard key={item.id} article={item}/>))}
+            </div>)}
+            </>)}
             <div className="mt-6 flex justify-end"><Pagination/></div>
         </Page>
     );
