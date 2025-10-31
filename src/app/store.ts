87501d6bc32b45b7b4c6dd95baa50454ce16de5a
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 20:24:20 -0400

    configuring/connecting slices to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import ui from "../features/articles/store/uiSlice";
+import favorites from "../features/articles/store/favoritesSlice";
+import ratings from "../features/articles/store/ratingSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { ui, favorites, ratings },
   devTools: true,
 });
 
