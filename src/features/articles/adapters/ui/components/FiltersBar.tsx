1ac2e85facb8b967807fc41044224b79f3b2deff
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 23:11:01 -0400

    getting subcategories based on categories, using useMemo and setting the query string to the value in the input

--- src/features/articles/adapters/ui/components/FiltersBar.tsx
+++ src/features/articles/adapters/ui/components/FiltersBar.tsx
@@ -1,7 +1,23 @@
+import { useMemo } from "react";
+import { useArticlesParams } from "../../../application/useArticlesParams";
+import { useListCategories } from '../../../application/useListCategories';
+
 export function FiltersBar() {
+    const {page, pageSize, q, categoryId, subcategoryId, set} = useArticlesParams();
+    const {data: categories, isLoading} = useListCategories();
+
+
+    const subcategories = useMemo(() => {
+        const cat = categories?.find(c => c.id == categoryId);
+        return cat?.subcategories ?? [];
+    }, [categories, categoryId]);
+    
     return (
-        <div className="flex flex-wrap items-center gap-2">
-            <input className="input input-bordered px-3 py-2 rounded-lg border w-64" placeholder="Search..."/>
+        <div className="flex flex-wrap items-end gap-3">
+            <div className="flex flex-col">
+                <label className="text-xs font-medium mb-1">Buscar</label>
+                <input value={q} onChange={(e) => set({q: e.target.value, page: 1})} className="px-3 py-2 rounded-lg border w-64" placeholder="Titulo o Contenido..."/>
+            </div>
         </div>
     );
 }
