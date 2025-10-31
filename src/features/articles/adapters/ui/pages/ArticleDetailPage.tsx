e518b2693910150aab5cf4f2789224b6125028a2
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-31 00:06:15 -0400

    updatin title

--- src/features/articles/adapters/ui/pages/ArticleDetailPage.tsx
+++ src/features/articles/adapters/ui/pages/ArticleDetailPage.tsx
@@ -3,7 +3,7 @@ import Page from "../../../../../shared/ui/Page";
 export default function ArticleDetailPage() {
     return (
         <Page>
-            <h1 className="text-2xl font-bold mb-44">Article Detail</h1>
+            <h1 className="text-2xl font-bold mb-44">Detalle de articulo</h1>
             {/* detalle del articulo con raiting favority etc*/}
         </Page>
     );
