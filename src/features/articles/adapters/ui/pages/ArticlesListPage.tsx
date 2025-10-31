efb9e58b9ebdbed4cfcbb053e8054a5b38a9681d
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 21:59:03 -0400

    adding test articles

--- src/features/articles/adapters/ui/pages/ArticlesListPage.tsx
+++ src/features/articles/adapters/ui/pages/ArticlesListPage.tsx
@@ -9,7 +9,12 @@ export default function ArticlesListPage() {
                 <h1 className="text-2xl font-bold">Articles</h1>
             </div>
             <div className="mb-4"><FiltersBar/></div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-4">data</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-4">
+                <div className="rounded-xl border bg-white p-4 shadow-sm">Articulo 1</div>
+                <div className="rounded-xl border p-4 bg-white shadow-sm">Articulo 2</div>
+                <div className="rounded-xl border p-4 bg-white shadow-sm">Articulo 3</div>
+                <div className="rounded-xl border p-4 bg-white shadow-sm">Articulo 4</div>
+            </div>
             <div className="mt-6 flex justify-end"><Pagination/></div>
         </Page>
     );
