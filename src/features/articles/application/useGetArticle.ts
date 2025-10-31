9e51b088712ba317bf48e8e13af65f6658d0a91b
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-31 00:47:50 -0400

    getting real article useGetArticle

--- src/features/articles/application/useGetArticle.ts
+++ src/features/articles/application/useGetArticle.ts
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import type { Article } from "../domain/types";
+import { articlesService } from "../adapters/http/articlesService";
 
 export function useGetArticle(_id: string): {
   data?: Article;
   isLoading: boolean;
   error?: unknown;
 } {
-  return { data: undefined, isLoading: false, error: undefined };
+  return useQuery<Article, Error>({
+    queryKey: ["article", _id],
+    queryFn: () => {
+      if (!_id) throw new Error("Falta el id");
+      return articlesService.get(_id);
+    },
+    enabled: !!_id,
+    staleTime: 60_000,
+  });
 }
