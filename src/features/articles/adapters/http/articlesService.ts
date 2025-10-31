e956dc5ad6b45075f0d9f4071b3c169ccfea5d2c
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 22:52:27 -0400

    implementing method of http adapter articlesService

--- src/features/articles/adapters/http/articlesService.ts
+++ src/features/articles/adapters/http/articlesService.ts
@@ -1,4 +1,4 @@
-import type { Paginated, Article } from "../../domain/types";
+import type { Paginated, Article, Category } from "../../domain/types";
 
 export interface ListParams {
   page: number;
@@ -18,20 +18,51 @@ export interface ArticlesServicePort {
     id: string,
     input: Partial<Omit<Article, "id" | "createdAt" | "updatedAt">>,
   ): Promise<Article>;
+  categories(): Promise<Category[]>;
 }
 
-// todo: implement functions
+function toQuery(params: Record<string, string | number | undefined> | ListParams) {
+  const sp = new URLSearchParams();
+  Object.entries(params).forEach(([k, v]) => {
+    if (v !== undefined && v !== "") sp.set(k, String(v));
+    return sp.toString();
+  });
+}
+
 export const articlesService: ArticlesServicePort = {
-  async list() {
-    return { items: [], total: 0, page: 1, pageSize: 10 };
+  async list(params) {
+    const queryString = toQuery(params);
+    const res = await fetch(`/api/articles?${queryString}`);
+    if (!res.ok) throw new Error(`List failed: ${res.status}`);
+    return res.json();
   },
-  async get() {
-    throw new Error("no-implemented");
+  async get(id: string) {
+    const res = await fetch(`/api/articles/${id}`);
+    if (!res.ok) throw new Error(`Get failed ${res.status}`);
+    return res.json();
   },
-  async create() {
-    throw new Error("no-implemented");
+  async create(input) {
+    const res = await fetch(`/api/articles`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(input),
+    });
+    if (!res.ok) throw new Error(`Create failed ${res.status}`);
+    return res.json();
   },
-  async update() {
-    throw new Error("no-implemented");
+  async update(id: string, input) {
+    const res = await fetch(`/api/articles/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(input),
+    });
+    if (!res.ok) throw new Error(`Update failed ${res.status}`);
+    return res.json();
+  },
+
+  async categories(): Promise<Category[]> {
+    const res = await fetch(`/api/categories`);
+    if (!res.ok) throw new Error(`Categories failed ${res.status}`);
+    return res.json();
   },
 };
