b5f3aa121e5296577459a59cccd887f87f52079d
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-10-30 23:49:22 -0400

    adding logic for pagination component can go next and can go back

--- src/features/articles/adapters/ui/components/Pagination.tsx
+++ src/features/articles/adapters/ui/components/Pagination.tsx
@@ -1,8 +1,26 @@
-export function Pagination() {
+type Props = {
+    page: number;
+    pageSize: number;
+    total: number;
+    onPageChange: (next: number) => void;
+}
+
+export function Pagination({ page, pageSize, total, onPageChange } : Props) {
+    const totalPages = Math.max(1, Math.ceil(total / pageSize));
+    const canGoBack = page > 1;
+    const canGoNex = page < totalPages;
     return (
         <div className="flex items-center gap-2">
-            <button className="px-3 py-1 rounded border hover:bg-gray-600 hover:text-white">Prev</button>
-            <button className="px-3 py-1 rounded border hover:bg-gray-600 hover:text-white">Next</button>
+            <button 
+            className="px-3 py-1 rounded border disabled:opacity-50 hover:bg-blue-600 hover:text-white"
+            disabled={!canGoBack}
+            onClick={() => onPageChange( page - 1)}
+            >Prev</button>
+            <button 
+            className="px-3 py-1 rounded border disabled:opacity-50 hover:bg-blue-600 hover:text-white"
+            disabled={!canGoNex}
+            onClick={() => onPageChange(page + 1)}
+            >Next</button>
         </div>
 
     );
