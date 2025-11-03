d143a887eea5c48f3b9bc61bd1db06d6d83df045
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-11-03 09:58:16 -0400

    creating slice and adding to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import ui from "./uiSlice";
 import favorites from "../features/articles/store/favoritesSlice";
 import ratings from "../features/articles/store/ratingSlice";
+import auth from "./authSlice";
 
 export const store = configureStore({
-  reducer: { ui, favorites, ratings },
+  reducer: { ui, favorites, ratings, auth },
   devTools: true,
 });
 
